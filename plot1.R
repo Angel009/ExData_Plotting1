549c18e55903062bb7a4735fdf2dd06bf8308670
Angel009 <angelus007.ans@gmail.com>
2021-03-03 20:22:40 -0600

    R code for the plot 1

--- plot1.R
+++ plot1.R
@@ -1,1 +1,19 @@
 ##R file for plot 1
+################################################
+#reading file and storing data
+consumption_file <- "household_power_consumption.txt"
+data <- read.table(consumption_file, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+#subsetting data for specific date
+dated_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+#formatting data for plot1
+activepower <- as.numeric(dated_data$Global_active_power)
+
+#creation of png file
+png("plot1.png", width=480, height=480)
+
+#plot structure
+hist(activepower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
